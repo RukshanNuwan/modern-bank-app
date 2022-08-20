e48a326a71e4e095d879f58cdcdcc711099442de
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-08-20 18:54:14 +0530

    creating a blueprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import './App.css';
+import styles from "./style.js";
 
 const App = () => {
   return (
-    <div className="p-5">
-      app
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          stats
+          business
+          billing
+          cardDeal
+          testimonial
+          clients
+          cta
+          footer
+        </div>
+      </div>
     </div>
   );
 };
