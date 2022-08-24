203bc89968d61e5858c66b772d2c36f624caa6d7
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-08-24 20:35:10 +0530

    created card deal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,6 +1,30 @@
+import {card} from '../assets';
+import styles, {layout} from "../style";
+import Button from './Button';
+
 const CardDeal = () => {
   return (
-    <div></div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/> in few easy steps.
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Alias, aliquid, amet atque consectetur
+          consequatur corporis debitis dignissimos ducimus illo natus, numquam officiis perspiciatis placeat porro quod
+          sint</p>
+
+        <Button styles="mt-10"/>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img
+          src={card}
+          alt="card"
+          className="w-[100%] h-[100%]"
+        />
+      </div>
+    </section>
   );
 };
 
